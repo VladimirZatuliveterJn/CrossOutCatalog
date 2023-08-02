2c10dcf1a1e1115a40b9f82d29fe84bb729ed9bc
Vladimir Zatuliveter Jn <vladimir.zatuliveter@gmail.com>
2023-08-02 12:25:15 +0600

    lowercase for data and data files.

--- main.js
+++ main.js
@@ -83,7 +83,7 @@ async function onSelect() {
             <div class="card" style="width: 18rem;">
                 <img src="${img}" class="card-img-top" alt="...">
                 <div class="card-body">
-                <h5 class="card-title">${item.name}</h5>
+                <h5 class="card-title">${item.displayName}</h5>
                 <p class="card-text">
                     <p>Resources:</p>
                     <ul>    
