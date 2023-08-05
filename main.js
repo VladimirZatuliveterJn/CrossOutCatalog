7cc00d6dab7cc5f49ba5f48dbc60b908bddbcdc7
Vladimir Zatuliveter Jn <vladimir.zatuliveter@gmail.com>
2023-08-05 12:39:28 +0600

    read child data init.

--- main.js
+++ main.js
@@ -12,26 +12,55 @@ function getSelectedValue(selectId) {
     return selectedValue
 }
 
-async function getParams(depencency) {
-    // example: "common/cabin/eger"
-    let parts = depencency.split('/'); // Split the string by '/'
-    objectName = parts.pop(); // Remove the last element from the array
+async function getItemWithChilds(fileName, itemName) {
     
-    fileName = parts.join('/') + '.json'
     jsonData = await getData(fileName)
     
     if (jsonData === undefined)
         throw new Error(`File is not found '${fileName}'.`)
 
-    object = findObjectByName(jsonData, objectName)
+    item = findObjectByName(jsonData, itemName)
     
-    if (object === null)
-        throw new Error(`Object '${objectName}' is not found in the file '${fileName}'.`)
+    if (item === null || item === undefined)
+        throw new Error(`Object '${itemName}' is not found in the file '${fileName}'.`)
     
-    console.log('getParams returned object =', object)
+    console.log('findObjectByName returned itemName = ', itemName, ', object =', item)
 
-    return object
+    item.iron = item.iron ?? 0
+    item.cuprum = item.cuprum ?? 0
+    item.plastic = item.plastic ?? 0
+    item.wire = item.wire ?? 0
+    item.shell = item.shell ?? 0
+    item.battery = item.battery ?? 0
+
+    if (item.dependency === undefined)
+    {
+        return item
+    }
+
+    for (const dependency of item.dependency)
+    {
+        console.log("dependecny:", dependency)
+        
+        // example: "common/cabin/eger"
+        let parts = dependency.split('/'); // Split the string by '/'
+        objectName = parts.pop(); // Remove the last element from the array
+        
+        childFileName = parts.join('/') + '.json'
+        
+        child = await getItemWithChilds(childFileName, objectName)
+
+        item.iron += child.iron ?? 0
+        item.cuprum += child.cuprum ?? 0
+        item.plastic += child.plastic ?? 0
+        item.wire += child.wire ?? 0
+        item.shell += child.shell ?? 0
+        item.battery += child.battery ?? 0
+    }
+
+    return item
 }
+
 
 function findObjectByName(data, name) {
     for (const obj of data) {
@@ -58,50 +87,31 @@ async function onSelect() {
     for (const item of json) {
         img = `./img/${rarity}/${moduleType}/${item.name}.webp`
         
-        console.log(item.name, item.dependency)
+        console.log(item.name)
 
-        item.iron = item.iron ?? 0
-        item.cuprum = item.cuprum ?? 0
-        item.plastic = item.plastic ?? 0
-        item.wire = item.wire ?? 0
-        item.shell = item.shell ?? 0
-        item.battery = item.battery ?? 0
+        fullItem = await getItemWithChilds(dataFileName, item.name)
 
-        if (item.dependency !== undefined)
-        {
-            for (const dependency of item.dependency)
-            {
-                console.log("dependecny:", dependency)
-
-                params = await getParams(dependency)
-                item.iron += params.iron ?? 0
-                item.cuprum += params.cuprum ?? 0
-                item.plastic += params.plastic ?? 0
-                item.wire += params.wire ?? 0
-                item.shell += params.shell ?? 0
-                item.battery += params.battery ?? 0
-            }
-        }
+        console.log('getItemWithChilds dataFileName = ', dataFileName, ', itemName = ', item.name, ', fullItem =', fullItem)
 
         cardHtml = `
             <div class="card" style="width: 18rem;">
                 <img src="${img}" class="card-img-top" alt="...">
                 <div class="card-body">
-                <h5 class="card-title">${item.displayName}</h5>
+                <h5 class="card-title">${fullItem.displayName}</h5>
                 <p class="card-text">
                     <p>Ресурсы:</p>
                     <ul>    
-                        <li>Железо: ${item.iron}</li>
-                        <li>Медь: ${item.cuprum}</li>
-                        <li>Пластик: ${item.plastic}</li>
-                        <li>Провода: ${item.wire}</li>
-                        <li>Аккумуляторы: ${item.battery}</li>
-                        <li>Гильзы: ${item.shell}</li>
+                        <li>Железо: ${fullItem.iron}</li>
+                        <li>Медь: ${fullItem.cuprum}</li>
+                        <li>Пластик: ${fullItem.plastic}</li>
+                        <li>Провода: ${fullItem.wire}</li>
+                        <li>Аккумуляторы: ${fullItem.battery}</li>
+                        <li>Гильзы: ${fullItem.shell}</li>
 
                     </ul>
                     <p>Зависит от:</p>
                     <ul>   
-                        ${item.dependency?.map(e => `<li>${e}</li>`)?.join('')}                                               
+                        ${fullItem.dependency?.map(e => `<li>${e}</li>`)?.join('')}                                               
                     </ul>                    
                 </p>
             </div>
